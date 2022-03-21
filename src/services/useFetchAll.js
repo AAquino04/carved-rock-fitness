2dcb6aac2e4f6ed72d8e090d669576ca79da9f3a
Anderson Aquino <johndoe@example.com>
2022-03-21 13:29:03 -0300

    fix infinite loop on fetch all hook

--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -1,11 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export default function useFetchAll(urls) {
+  const prevUrls = useRef([]);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    // Only run if URLs array changes
+    if (areEqual(prevUrls.current, urls)) {
+      setLoading(false);
+      return;
+    }
+    prevUrls.current = urls;
+
     const promises = urls.map((url) =>
       fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
         if (response.ok) return response.json();
@@ -20,8 +28,14 @@ export default function useFetchAll(urls) {
         setError(e);
       })
       .finally(() => setLoading(false));
-    // eslint-disable-next-line
-  }, []);
+  }, [urls]);
 
   return { data, loading, error };
+}
+
+function areEqual(arr1, arr2) {
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((value, index) => value === arr2[index])
+  );
 }
