1f2877163a027d5b1cb8d9b5d4a8b871c0495f03
Anderson Aquino <johndoe@example.com>
2022-03-21 13:20:55 -0300

    fix setting state in unmounted component error

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 
 function useFetch(url) {
+    const isMounted = useRef(false);
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -8,22 +9,25 @@ function useFetch(url) {
     const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
     useEffect(() => {
+        isMounted.current = true;
         async function init() {
             try {
                 const response = await fetch(baseUrl + url);
                 if (response.ok) {
                     const json = await response.json();
-                    setData(json);
+                    if (isMounted.current) setData(json);
                 } else {
                     throw response;
                 }
             } catch (err) {
-                setError(err);
+                if (isMounted.current) setError(err);
             } finally {
-                setLoading(false);
+                if (isMounted.current) setLoading(false);
             }
         }
         init();
+
+        return () => isMounted.current = false;
     }, [baseUrl, url]); // Executes after url changes
 
     return { data, error, loading }
